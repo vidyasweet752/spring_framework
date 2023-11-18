bf30572130625e14c2a4cbf8916ae0eaea9bcb44
vidyasweet752 <vidyasweet752@gmail.com>
2023-11-18 10:59:06 +0530

    Initial Commit

---
+++ src/test/java/com/example28minutes/learnspringframework/LearnSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example28minutes.learnspringframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
